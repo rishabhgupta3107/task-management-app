d3d3ee002743ae56c73ff4172c7af0a723de513d
Rishabh Gupta <hrrishu0001@gmail.com>
2024-11-18 15:38:20 +0530

    Model class fix

--- backend-springboot/src/main/java/com/rishabhgupta3107/taskmanagement/backend_springboot/model/Task.java
+++ backend-springboot/src/main/java/com/rishabhgupta3107/taskmanagement/backend_springboot/model/Task.java
@@ -1,5 +1,8 @@
 package com.rishabhgupta3107.taskmanagement.backend_springboot.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -8,10 +11,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Data
@@ -34,11 +38,39 @@ public class Task {
   @Enumerated(EnumType.STRING)
   private Status status;
 
+  @JsonFormat(pattern = "yyyy-MM-dd")
   private LocalDate dueDate;
 
   public enum Status {
-    TO_DO,
-    IN_PROGRESS,
-    DONE
+    TO_DO("TO_DO"),
+    IN_PROGRESS("IN_PROGRESS"),
+    DONE("DONE");
+
+    private final String value;
+
+    Status(String value){
+      this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+      return value;
+    }
+
+    @Override
+    public String toString() {
+      return this.value;
+    }
+
+    @JsonCreator
+    public static Status fromValue(String value) {
+      for (Status status : Status.values()) {
+        if (status.value.equalsIgnoreCase(value)) {
+          return status;
+        }
+      }
+      throw  new IllegalArgumentException("Invalid Status");
+    }
+
   }
 }
